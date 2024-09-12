ef10ac9fd132c0a837d728349527003b9217f612
Olexandra <olexandra.dmytrenko@gmail.com>
2024-09-12 09:37:56 +0200

    * added methods into the Car and UseCar for driving

--- oop/src/lecture1/carexample/Car.java
+++ oop/src/lecture1/carexample/Car.java
@@ -4,8 +4,29 @@ package lecture1.carexample;
 public class Car {
     private String model;
     private int year;
+    private int fuelLevel;
 
-    public void drive() {
-        System.out.println("Car is driving");
+    public Car() {
+        fuelLevel = 100;
+    }
+
+    public Car(int fuelLevelNew) {
+        this.fuelLevel = fuelLevelNew;
+    }
+
+    public void drive(int distance) {
+        while (distance >= 10) {
+            distance = distance - 10;
+            fuelLevel--;
+        }
+        System.out.println("Car is driving. Fuel level is " + fuelLevel);
+    }
+
+    public void setFuelLevel(int fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public int getFuelLevel() {
+        return fuelLevel;
     }
 }
--- oop/src/lecture1/carexample/UseCar.java
+++ oop/src/lecture1/carexample/UseCar.java
@@ -5,9 +5,12 @@ public class UseCar {
     Car someCar;  // someCar має тип Car, але поки що не є об'єктом
     public static void main(String[] args) {
         // Використання
-        Car myCar = new Car();  // myCar - це об'єкт класу Car
+        Car myCar = new Car(60);  // myCar - це об'єкт класу Car
         Car yourCar = new Car(); // yourCar - інший об'єкт того ж класу
 
-        myCar.drive();
+        myCar.drive(20);
+        myCar.setFuelLevel(myCar.getFuelLevel() + 10);
+
+        System.out.println(myCar.getFuelLevel());
     }
 }
